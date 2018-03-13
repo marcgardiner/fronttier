fe12632575fbd7b0efb277dc785df5542c4e946e
usman-hussain <usmanh@marketlytics.com>
2018-03-13 13:13:31 +0500

    hiring dashboard css fixes

--- web/ng_app/src/app/auth/login/login.component.ts
+++ web/ng_app/src/app/auth/login/login.component.ts
@@ -15,7 +15,7 @@ export class LoginComponent implements OnInit {
     name: new FormControl('', [Validators.required, Validators.minLength(2)]),
     lastname: new FormControl(''),
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.minLength(8)])
+    password: new FormControl('', [Validators.required, Validators.minLength(5)])
   });
 
   authTitle: string;
