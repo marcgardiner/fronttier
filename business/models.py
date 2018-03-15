f37ec6e2bd88fba8ecf78520c2e27990955f5b39
Usman Masood <usmanm@fastmail.fm>
2018-03-15 00:20:21 -0700

    username required

--- business/models.py
+++ business/models.py
@@ -29,7 +29,7 @@ class User(BaseModel, AbstractUser, AddressFields):
             (HIRING_MANAGER, 'Hiring Manager')
         )
 
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username']
     REGISTRATION_FIELDS = ['first_name', 'last_name']
 
     type = models.CharField(choices=Type.CHOICES, max_length=16)
